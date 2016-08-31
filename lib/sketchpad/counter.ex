114d592106cff7f04b9c412ce0fce79cf27f5202
Mathieu Gagne <gagne.mathieu@hotmail.com>
2016-08-31 14:04:56 -0400

    Counter as GenServer

--- lib/sketchpad/counter.ex
+++ lib/sketchpad/counter.ex
@@ -1,55 +1,32 @@
 defmodule Sketchpad.Counter do
-
-  ## USAGE
-
-  # iex(1)> alias Sketchpad.Counter 
-  # Sketchpad.Counter
-  # iex(2)> {:ok, pid} = Counter.start_link
-  # {:ok, #PID<0.225.0>}
-  # iex(3)> Counter.inc(pid)
-  # :inc
-  # iex(4)> Counter.inc(pid)
-  # :inc
-  # iex(5)> Counter.inc(pid)
-  # :inc
-  # iex(6)> Counter.inc(pid)
-  # :inc
-  # iex(7)> Counter.val(pid)
-  # 4
-  # iex(8)> Counter.dec(pid)
-  # :dec
-  # iex(9)> Counter.val(pid)
-  # 3
+  use GenServer
 
   ## Client
 
-  def inc(pid), do: send(pid, :inc)
-  def dec(pid), do: send(pid, :dec)
-  def val(pid, timeout \\ 5000) do
-    # Guaranteed unique value
-    ref = make_ref()
-    send(pid, {:val, self(), ref})
-    receive do
-      {:val, ^ref, val} -> val
-    after timeout -> exit(:timeout)
-    end
-  end
+  def inc(pid), do: GenServer.cast(pid, :inc)
+  def dec(pid), do: GenServer.cast(pid, :dec)
+  def val(pid), do: GenServer.call(pid, :val)
   
   ## Server
 
   def start_link(initial_count \\ 0) do
-    {:ok, spawn_link(fn -> listen(initial_count) end)}
+    GenServer.start_link(__MODULE__, initial_count)
   end
 
-  # Recursive allows us to keep state of count
-  defp listen(count) do
-    receive do
-      :inc -> listen(count + 1)
-      :dec -> listen(count - 1)
-      {:val, sender, ref} ->
-        send(sender, {:val, ref, count})
-        listen(count)
-    end
+  def init(initial_count) do
+    {:ok, initial_count}
+  end
+
+  def handle_cast(:inc, count) do
+    {:noreply, count + 1}
+  end
+
+  def handle_cast(:dec, count) do
+    {:noreply, count - 1}
+  end
+
+  def handle_call(:val, _from, count) do
+    {:reply, count, count}
   end
 
 end
